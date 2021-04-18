7749b6d62148d3a6478c01a07f62eb060c86cd95
ILikeSahne <ILikeSahne@gmx.at>
2021-04-18 03:00:41 +0200

    Started RSA encryption/decryption

---
+++ InventarAPI/InventarAPI/APITest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace InventarAPI
+{
+    class APITest
+    {
+        private const string domain = "ilikesahne.ddns.net";
+        private const int port = 10001;
+
+        static void Main(string[] args)
+        {
+            /*InventarAPI api = new InventarAPI(domain, port);
+            APIError e = api.OpenConnection();
+            if(!e)
+            {
+                e.PrintError();
+            } else
+            {
+                InventarAPI.WriteLine("Connection established!");
+            }*/
+            
+        }
+    }
+}
